ae669c5fe800549710746059f58b300e16613970
Alex Wulf <aw@dc.ag>
2023-01-20 17:14:42 +0100

    WIP

--- src/Discord/SDK/Types/ApplicationCommandObject.php
+++ src/Discord/SDK/Types/ApplicationCommandObject.php
@@ -4,7 +4,88 @@ namespace Wulfheart\LaravelDiscord\Discord\SDK\Types;
 
 class ApplicationCommandObject
 {
-    public Snowflake $id;
-    public ApplicationCommandTypeEnum $type;
-    public Snowflake $application_id;
+    protected Snowflake $id;
+    protected ApplicationCommandTypeEnum $type;
+    protected Snowflake $application_id;
+    protected Snowflake $guild_id;
+    protected string $name;
+    protected string $description;
+
+
+    public function getId(): Snowflake
+    {
+        return $this->id;
+    }
+
+
+    public function setId(Snowflake $id): ApplicationCommandObject
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+
+    public function getType(): ApplicationCommandTypeEnum
+    {
+        return $this->type;
+    }
+
+
+    public function setType(ApplicationCommandTypeEnum $type): ApplicationCommandObject
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+
+    public function getApplicationId(): Snowflake
+    {
+        return $this->application_id;
+    }
+
+
+    public function setApplicationId(Snowflake $application_id): ApplicationCommandObject
+    {
+        $this->application_id = $application_id;
+        return $this;
+    }
+
+
+    public function getGuildId(): Snowflake
+    {
+        return $this->guild_id;
+    }
+
+
+    public function setGuildId(Snowflake $guild_id): ApplicationCommandObject
+    {
+        $this->guild_id = $guild_id;
+        return $this;
+    }
+
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+    public function setName(string $name): ApplicationCommandObject
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+
+    public function setDescription(string $description): ApplicationCommandObject
+    {
+        $this->description = $description;
+        return $this;
+    }
 }
