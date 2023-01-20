3fd667a01ac98eb1e68880c02f2915decb0784ba
Alex Wulf <aw@dc.ag>
2023-01-21 00:04:20 +0100

    WIP

--- src/Discord/SDK/Requests/BulkOverwriteGlobalApplicationCommandsRequestTest.php
+++ src/Discord/SDK/Requests/BulkOverwriteGlobalApplicationCommandsRequestTest.php
@@ -2,8 +2,6 @@
 
 namespace Wulfheart\LaravelDiscord\Discord\SDK\Requests;
 
-use Saloon\Http\Faking\MockClient;
-use Saloon\Http\Faking\MockResponse;
 use Wulfheart\LaravelDiscord\Discord\SDK\DiscordApiConnector;
 use Wulfheart\LaravelDiscord\Discord\SDK\Types\SlashApplicationCommandObject;
 use Wulfheart\LaravelDiscord\Tests\TestCase;
@@ -14,13 +12,9 @@ class BulkOverwriteGlobalApplicationCommandsRequestTest extends TestCase
     {
         $connector = new DiscordApiConnector();
 
-        $mockClient = new MockClient([
-            BulkOverwriteGlobalApplicationCommandsRequest::class => MockResponse::fixture('bulk-overwrite-1'),
-        ]);
-
         $command = SlashApplicationCommandObject::make()->setName("test3")->setDescription("test");
         $request = new BulkOverwriteGlobalApplicationCommandsRequest([$command]);
-        $res = $connector->send($request, $mockClient);
+        $res = $connector->send($request);
 
         $this->assertTrue($res->ok());
     }
